48edeebd83c7e4aac3c53b63f2a03ae0b915b6c7
mmorley <mmorley0395@gmail.com>
2022-06-22 16:20:52 -0400

    changed post endpoint

--- app/main.py
+++ app/main.py
@@ -5,21 +5,27 @@ from fastapi.staticfiles import StaticFiles
 from tmc_summarizer import write_summary_file
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-# @app.post("/path/")
-# async def path(path: str = Form()):
-#     return {"username": path}
+@app.post("/path/")
+async def path(path: str = Form()):
+    return {"Filepath": path}
+
+
+# app.mount(
+#     "/Volumes/GoogleDrive/Shared drives",
+#     StaticFiles(directory="Volumes"),
+#     name="shared",
+# )
 
 
 @app.post("/files/")
-async def create_files(files: List[bytes] = File()):
+async def create_files(files: list[bytes] = File()):
     return {"file_sizes": [len(file) for file in files]}
 
 
 @app.post("/uploadfiles/")
-async def create_upload_files(files: List[UploadFile]):
+async def create_upload_files(files: list[UploadFile]):
     return {"filenames": [file.filename for file in files]}
 
 

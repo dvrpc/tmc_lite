7292408b3326887c438426fe2180cd3c0bbe0e53
Mark Morley <mmorley0395@gmail.com>
2022-06-23 13:26:14 -0400

    added aiofiles

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from tmc_summarizer import write_summary_file
+import aiofiles
 
 app = FastAPI()
 
@@ -23,9 +24,9 @@ Upload your TMC Files here!
 File names must meet the following criteria:
 <h2/>
 <ul>
-        <li> file ends in ``.xls``</li>
-        <li> filename has at least 1 underscore</li>
-        <li> text before the first underscore is a number</li>
+        <li> File ends in `.xls`</li>
+        <li> Filename has at least 1 underscore</li>
+        <li> Text before the first underscore is a number</li>
 <ul/>
 <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
 <input name="files" type="file" multiple>

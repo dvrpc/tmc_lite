2b96c6009c5a171844900e62760505de350de860
Mark Morley <mmorley0395@gmail.com>
2025-01-02 13:41:33 -0500

    add documentation

--- app/main.py
+++ app/main.py
@@ -14,7 +14,9 @@ app = FastAPI()
 
 
 @app.post(f"{URL_PATH}/uploadfiles/")
-async def create_upload_files(files: list[UploadFile], background_tasks: BackgroundTasks):
+async def create_upload_files(
+    files: list[UploadFile], background_tasks: BackgroundTasks
+):
     # copies the files from the user directory into this repo, runs function from tmc_summarizer, returns summary file, cleans out excel files
     delete_excel()
     for file in files:
@@ -75,6 +77,13 @@ File names must meet the following criteria:
         <li> File ends in `.xls`</li>
         <li> Filename has at least 1 underscore</li>
         <li> Text before the first underscore is a number</li>
+        <li> It is highly recommended that the numbers "e.g., the '3' in 3_DVRPC-TMC....xls" correspond to the node/intersection numbers in Synchro, to keep your intersections organized. 
+        <li> Example filenames: 
+            <ul>
+                <li> 1_DVRPC-TMC-174985 - Erie Ave @ PA 611 Broad St.xls
+                <li> 2_DVRPC-TMC-174986 - Erie Ave @ Germantown Ave.xls
+                <li> 3_DVRPC-TMC-175011 - Erie Ave @ Kensington Ave.xls
+            <ul/>
 <ul/>
 <p>
 Summary file download takes a few seconds after pressing submit..
